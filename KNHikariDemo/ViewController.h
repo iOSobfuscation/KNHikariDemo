4f609b9750cb0481e91d55991f7df63cf110e7f7
zhangkn <developerkunnan@gmail.com>
2018-06-07 08:31:17 +0800

    first commit

---
+++ KNHikariDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNHikariDemo
+//
+//  Created by devzkn on 07/06/2018.
+//  Copyright © 2018 devzkn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KNHikariDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KNHikariDemo
+//
+//  Created by devzkn on 07/06/2018.
+//  Copyright © 2018 devzkn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
